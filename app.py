c6674b24caf758a779cf30c8e25d59112cfc69aa
Katherine Barbano <t-kabarb@microsoft.com>
2020-06-28 08:14:57 -0700

    added jinja for user match page

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, request, render_template, redirect, url_for
 from dotenv import load_dotenv
 load_dotenv()
 
-global number#fix global
-number=0
 
 app = Flask(__name__)
 
@@ -36,14 +34,58 @@ def rank():
     if request.method == "GET":#somehow need to get these arrays + database info into rankings.html table info
         return render_template("rankings.html")
     elif request.method == "POST":
-        #number=request.form['rankings']
-        return redirect(url_for('userconnect'))
+        num=request.form['num']
+        if num=="1":
+            return redirect(url_for('userconnect1'))
+        elif num=="2":
+            return redirect(url_for('userconnect2'))
+        elif num=="3":
+            return redirect(url_for('userconnect3'))
+        elif num=="4":
+            return redirect(url_for('userconnect4'))
+        elif num=="5":
+            return redirect(url_for('userconnect5'))
 
-@app.route('/userconnect/', methods=['GET','POST'])
-def selectedUserInfo():
-    #if request.method=='GET':
-    #    return render_template("userconnect.html")
-    return number#need to display the correct user
+@app.route('/userconnect1/')
+@app.route('/userconnect1/<uname>')
+def userconnect1():
+    #pull info from database
+    user="user1"#eg database[1]
+    goal="goal1"#eg database[1]
+    return render_template('userconnect.html',uname=user,goal=goal)
+
+@app.route('/userconnect2/', methods=['GET','POST'])
+@app.route('/userconnect2/<uname>')
+def userconnect2():
+    #pull info from database
+    user="user2"#eg database[1]
+    goal="goal2"#eg database[1]
+    return render_template('userconnect.html',uname=user,goal=goal)
+
+@app.route('/userconnect3/', methods=['GET','POST'])
+@app.route('/userconnect3/<uname>')
+def userconnect3():
+    #pull info from database
+    user="user3"#eg database[2]
+    goal="goal3"#eg database[2]
+    return render_template('userconnect.html',uname=user,goal=goal)
+
+@app.route('/userconnect4/', methods=['GET','POST'])
+@app.route('/userconnect4/<uname>')
+def userconnect4():
+    #pull info from database
+    user="user4"#eg database[3]
+    goal="goal4"#eg database[3]
+    return render_template('userconnect.html',uname=user,goal=goal)
+
+@app.route('/userconnect5/', methods=['GET','POST'])
+@app.route('/userconnect5/<uname>')
+def userconnect5():
+    #pull info from database
+    user="user5"#eg database[4]
+    goal="goal5"#eg database[4]
+    return render_template('userconnect.html',uname=user,goal=goal)
+
     
 
 @app.route('/signup/', methods=['GET', 'POST'])
