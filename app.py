b448ea9272b9e25e47c3abea464532c472555a0b
Jachimike Onuoha <t-jaonuo@microsoft.com>
2020-06-27 20:59:04 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, request, render_template
 
+
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return "Hello World Jachi" 
+    return "Hello World Jachi"
