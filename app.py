a2776a9493e67250bc80910832f224f03aa722ec
Rishi Tekriwal <rtekriwal1927@gmail.com>
2020-06-27 21:57:08 -0400

    signup

--- app.py
+++ app.py
@@ -13,8 +13,17 @@ def login():
         return render_template('login.html')
     elif request.method == 'POST':
         # User has sent us data
-        return render_template('index.html')
+        return redirect(url_for('index'))
 
 @app.route('/index')
 def index():
 	return "Good you have logged in"
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        # User is requesting the form
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        # User has sent us data
+        return redirect(url_for('index'))
